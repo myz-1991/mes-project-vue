042558505d287a9d61360a0b5257f95fb4a16b28
myz <myz@lgcx.com>
2020-11-24 11:50:29 +0800

    首页修改

--- src/views/plan/workJobManage/workJobManage.vue
+++ src/views/plan/workJobManage/workJobManage.vue
@@ -102,19 +102,19 @@ export default {
       },
       tableData: [{
         date: '2016-05-02',
-        name: '王小虎',
+        name: '张三',
         address: '上海市普陀区金沙江路 1518 弄'
 	  }, {
         date: '2016-05-04',
-        name: '王小虎',
+        name: '李四',
         address: '上海市普陀区金沙江路 1517 弄'
 	  }, {
         date: '2016-05-01',
-        name: '王小虎',
+        name: '王五',
         address: '上海市普陀区金沙江路 1519 弄'
 	  }, {
         date: '2016-05-03',
-        name: '王小虎',
+        name: '赵六',
         address: '上海市普陀区金沙江路 1516 弄'
 	  }],
       fileList: [],
